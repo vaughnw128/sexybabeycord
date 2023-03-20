446eaae4a379156ca7cdea4eae508ae6d646f413
Vaughn Woerpel <vaughnwoerpel@gmail.com>
2023-03-20 00:45:14 -0400

    Added documentation to statcat and sexybabeycord

--- cogs/statcat.py
+++ cogs/statcat.py
@@ -1,92 +1,153 @@
+""" Statcat (A Sexybabeycord Cog)
+
+    This cog is an isolated portion of the main Sexybabeycord bot.
+    It's primary function is to generate statistics based on cached messages
+    from a discord server, then display those stats to users in an easy-to-read manner.
+"""
+
 import discord
 from discord import app_commands
-from discord.ext import commands, tasks
-import pytz
+from discord.ext import commands
 import datetime
 import json
 import os
+import time
 from typing import Optional
 
 class Statcat(commands.Cog):
+    """ A Discord Cog to handle all of the statistic-generating functionalities of the Sexybabeycord bot.
+
+        ...
+
+        Attributes
+        ----------
+        bot: commands.Bot
+            The bot object from the main cog runner
+
+        Methods
+        -------
+        loadmessages(interaction, startdate, enddate)
+            Loads messages within the date range to json data files.
+    """
+
     def __init__(self, bot: commands.Bot):
+        """ Initializes the cog.
+
+            Parameters
+            -----------
+            bot: commands.Bot
+                The bot object from the main cog runner.
+        """
         self.bot = bot
-        self.batch = []
-        self.save_batch.start()
-
-    @tasks.loop(minutes=5.0)
-    async def save_batch(self):
-        
-        if not os.path.exists(f"data/messages-{datetime.date.today()}.json"):
-            with open(f"data/messages-{datetime.date.today()}.json", 'w') as messages_json:
-                filler = {"messages" : []}
-                json.dump(filler, messages_json, indent=4)
-
-        with open(f"data/messages-{datetime.date.today()}.json", "r") as messages_json:
-            day_data = json.load(messages_json)
-            combined_batch = day_data['messages'] + self.batch
-            messages_json.close()
-
-        dump = {"messages": combined_batch}
-
-        with open(f"data/messages-{datetime.date.today()}.json", "w") as messages_json:
-            json.dump(dump, messages_json, indent=4)
-            messages_json.close()
-        self.batch = []
     
     @app_commands.command(name="loadmessages")
     async def loadmessages(self, interaction: discord.Interaction, startdate: str=None, enddate: str=None):
-        r""" Loads messages into json files based on user-supplied dates
+        """ Loads messages into json files based on user-supplied dates
         
-        Attributes
-        -----------
-        startdate: Optional[:class:`str`]
-            The start date of the date range
-        enddate: Optional[:class:`str`]
-            The end date of the date range
+            Takes in either a date range, a date, or nothing (current date), and then caches all messages
+            from that date range to the /data directory. It skips over dates that have already been done,
+            and saves specific attributes of the message for further analysis.
+
+            ...
+
+            Parameters
+            -----------
+            startdate: Optional[str]
+                The start date of the date range
+            enddate: Optional[str]
+                The end date of the date range
         """
         
+        # Changes the specified dates to datetime
         startdate = to_datetime(startdate)
         enddate = to_datetime(enddate)
 
+        # Horrendous if statement that decides how the dates should be arranged for the upcoming message history query
         if startdate is None and enddate is None:
-            startdate = datetime.datetime.today()
-            enddate = datetime.datetime.today()
+            startdate = datetime.datetime.today().replace(hour=0, minute=0, second=0, microsecond=0)
+            enddate = datetime.datetime.today().replace(hour=0, minute=0, second=0, microsecond=0)
         elif startdate is not None and enddate is None:
             enddate = startdate
         elif startdate is None and enddate is not None:
             startdate = enddate
-        # else:
-        #     enddate = enddate+datetime.timedelta(days=1)
 
-        startdate = startdate-datetime.timedelta(days=1)
-        
+        #Creates a list of dates between the inputs provided. If it's empty, set it to the startdate. If not, add the end date to cap it off
         dates = [startdate+datetime.timedelta(days=x) for x in range((enddate-startdate).days)]
-        dates.append(dates[len(dates)-1]+datetime.timedelta(days=1))
+        if len(dates) == 0:
+            dates.append(startdate)
+        else:
+            dates.append(enddate)
+
+        #Lets the user know what's going on
+        await interaction.response.send_message(f"Processing... This may take a moment!")
         
+        # Starts a timer and a message count for later output
+        start = time.time()
+        message_count = 0
+        
+        # Loops along the date range and runs the message history
         for date in dates:
+            # If the file exists already, skip over that date. This saves time for future operations if the date doesn't need to be indexed
+            if os.path.exists(f"data/messages-{date.date()}.json"):
+                continue
             messages = []
+
+            # Gets the message history for all text channels on the specified date
             for channel in interaction.guild.channels:
                 if type(channel) is discord.TextChannel: 
-                    print(f"Channel: {channel.name}\nStartdate: {date}\nEnddate: {date+datetime.timedelta(days=1)}")
                     messages += [message async for message in channel.history(limit=None, after=date, before=date+datetime.timedelta(days=1), oldest_first=True)]
-            messages_to_json(messages)
-    
-    @app_commands.command(name="ping")
-    async def ping(self, interaction: discord.Interaction):
-        await interaction.response.send_message("Pong!")
+            message_count += len(messages)
+
+            # Translates the messages to a dict, then puts it into a json file for that date
+            messages_to_json(messages, date)
+
+        # Ends the timer then outputs the results to the user
+        end = time.time()
+        await interaction.channel.send(f"Cached {message_count} from {str(startdate.date())} to {str(enddate.date())} in {round(end-start, 2)} seconds.")
+
 
 def to_datetime(date) -> Optional[datetime.datetime]:
-    """Formats the date string to a datetime object"""
+    """ Formats the date string to a datetime object
+
+        Parameters
+        -----------
+        date: str
+            Date formatted as a string
+        
+        Returns
+        -----------
+        Optional[datetime.datetime]
+            A formatted datetime.datetime object derived from the input string
+    
+    """
     if date is not None:
         date = datetime.datetime.strptime(date, "%m-%d-%Y")
 
     return date
 
-def messages_to_json(messages):
+
+def messages_to_json(messages, date):
+    """ Formats messages into dictionary/json format and writes them to a file.
+
+        Takes the input from the loadmessages() command, then outputs them into a file labeled with the provided date.
+
+        ...
+
+        Parameters
+        -----------
+        messages: list of str
+            List of messages to format and write to file.
+        date: datetime.datetime
+            Datetime.datetime object of the current date index of the calling for loop.
+    """
+
+    # Initializes and empty dict and then loops through messages
+    dict_to_json = {"messages": []}
     for message in messages:
+        # Special formatting for the message dict, altering things like whether the message has a gif or not, and who the message mentions
         message_dict = {
             "message_id" : message.id,
-            "timestamp" : str(message.created_at.replace(tzinfo=pytz.utc).astimezone(pytz.timezone("America/New_York"))),
+            "timestamp" : str(message.created_at),
             "channel" : message.channel.id,
             "author" : message.author.id,
             "content" : message.content,
@@ -94,9 +155,28 @@ def messages_to_json(messages):
             "gif" : (lambda a: True in a)([x in message.content for x in ("https://tenor.com/view", "https://giphy.com/gifs", ".gif")]),
             "mentions" : [x.id for x in message.mentions]
         }
+        dict_to_json["messages"].append(message_dict)
 
-        print(message_dict)
+    # If the dict is empty simply return and do not write to a file
+    if len(dict_to_json["messages"]) == 0:
+        return
+    
+    # Checks if the file exists, and if it does not, dumps the data to the file
+    if not os.path.exists(f"data/messages-{date.date()}.json"):
+        with open(f"data/messages-{date.date()}.json", 'w') as messages_json:
+            json.dump(dict_to_json, messages_json, indent=4)
+    else:
+        return
 
 async def setup(bot: commands.Bot):
+  """ Sets up the cog
+
+     Parameters
+     -----------
+     bot: commands.Bot
+        The main cog runners commands.Bot object
+  """
+
+  # Adds the cog and reports that it's loaded
   await bot.add_cog(Statcat(bot))
   print("Statcat: I'm loaded 😼")
--- sexybabeycord.py
+++ sexybabeycord.py
@@ -1,12 +1,11 @@
 import discord
 from discord.ext import commands
 from typing import Literal, Optional
-from discord import app_commands
-from config import token, guild
+from config import token
 import os
 from discord.ext.commands import Greedy, Context
-import logging
 
+# Defines the bot intents and the client
 intents = discord.Intents.all()
 client = commands.Bot(command_prefix='$', intents=intents)
 
@@ -15,6 +14,22 @@ client = commands.Bot(command_prefix='$', intents=intents)
 @commands.is_owner()
 async def sync(
   ctx: Context, guilds: Greedy[discord.Object], spec: Optional[Literal["~", "*", "^"]] = None) -> None:
+    r""" Loads messages into json files based on user-supplied dates
+        
+        Syncing to the command tree is important as this is how the guild populates itself with the discord app_commands.
+
+        ...
+
+        Parameters
+        -----------
+        ctx: Context
+            Command context object
+        guilds: Optional[discord.Object]
+            List of guilds
+        spec: Optional[Literal["~", "*", "^"]
+            Optional specification for what the sync command should do: sync the tree, copy the tree, or clear the tree.
+        """
+
     if not guilds:
         if spec == "~":
             synced = await ctx.bot.tree.sync(guild=ctx.guild)
@@ -44,18 +59,22 @@ async def sync(
 
     await ctx.send(f"Synced the tree to {ret}/{len(guilds)}.")
 
-# Load all of the cogs
-async def load_extensions():
-    # await client.load_extension("cogs.statcat")
-    # await client.load_extension("cogs.astropix")
+async def load_cogs():
+    """ Loads all of the cogs in the /cogs directory.
+    
+        Cogs allow for a more streamlined process of managing a large bot.
+    """
     for filename in os.listdir("./cogs"):
         if filename.endswith(".py"):
-            # cut off the .py from the file name
             await client.load_extension(f"cogs.{filename[:-3]}")
 
 @client.event
 async def on_ready():
+    """ Discord client event for when the bot is ready
+    
+        Runs as soon as the bot starts
+    """
     print("Sexybabeycord: Now I'm all spooled up!")
-    await load_extensions()
+    await load_cogs()
 
 client.run(token)
