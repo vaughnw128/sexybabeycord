eccb84fd784208d53191278ad45b24f5eafbc882
vww9966 <vww9966@rit.edu>
2023-03-23 17:32:47 -0400

    Added embeds

--- cogs/statcat.py
+++ cogs/statcat.py
@@ -85,8 +85,7 @@ class Statcat(commands.Cog):
     @app_commands.rename(date2="end-date")
     async def statcat(
         self, 
-        interaction: 
-        discord.Interaction, 
+        interaction: discord.Interaction, 
         option: Literal['message', 'word', 'image', 'gif', 'mention'], 
         search: Optional[str]=None,
         user: Optional[str]=None, 
@@ -120,46 +119,71 @@ class Statcat(commands.Cog):
         dates = await date_handler(interaction, date1, date2)
         if dates is None:
             return
-        end = time.time()
-        print(f"date_handler: {end-start}")
 
-        start = time.time()
         messages = await load_messages(interaction, dates)
-        end = time.time()
-        print(f"load_messages: {end-start}")
-
-        start = time.time()
-        stats = generate_stats(search, messages, user)
-        end = time.time()
-        print(f"generate_stats: {end-start}")
-
-        await interaction.followup.send(stats)
         
+        stats = generate_stats(search, messages, user)
+        
+        end = time.time()
+        runtime = round(end-start, 2)
+        embed = generate_embed(interaction, stats, search, user, dates, runtime)
+        await interaction.channel.send(embed=embed)
+        await interaction.followup.send("Done!")
+        
+def generate_embed(interaction, stats, search, user, dates, runtime):
+    embed=discord.Embed(title="Statistics", description=f"Message statistics for {interaction.guild} between {dates[0].date()} and {dates[len(dates)-1].date()}", color=0x6de0bd)
+    if user is not None:
+        embed.add_field(name="User", value=user, inline=True)
+    embed.add_field(name="Messages", value=stats["message_count"], inline=True)
+    if search is not None:
+        embed.add_field(name="String", value=search, inline=True)
+    embed.add_field(name="Images", value=stats["image_count"], inline=True)
+    embed.add_field(name="Gifs", value=stats["gif_count"], inline=True)
+    embed.add_field(name="Author", value=stats["authors"][0], inline=True)
+    embed.add_field(name="Date", value=stats["dates"][0], inline=True)
+    embed.add_field(name="Mentions", value=stats["mentions"][0], inline=True)
+    embed.set_footer(text=f"Generated in {runtime} seconds.")
+    return embed
+
 def generate_stats(search, messages, user):
     stats = {
-        "messages": 0,
-        "words": 0,
-        "images": 0,
-        "gifs": 0,
+        "message_count": 0,
+        "word_count": 0,
+        "image_count": 0,
+        "gif_count": 0,
         "mentions": {},
         "authors": {},
-        "dates": {}
+        "dates": {},
+        "gifs": {}
     }
 
     for date in messages:
         for message in messages[date]:
-            inc = 1
+            inc, wc = 1, 1
+
+            if user is not None:
+                if str(message["author"]) != user[2:len(user)-1]:
+                    inc = 0
+
             if search is not None:
-                wc = len(re.findall(r"\b"+re.escape(search)+r"\b", message["content"].lower()))
-                stats["words"] += wc
+                wc = len(re.findall(r"\b"+re.escape(search.lower())+r"\b", message["content"].lower()))
+                stats["word_count"] += wc*inc
                 if wc == 0:
                     inc = 0
 
-            stats["messages"] += inc
-            stats["images"] += message["num_attachments"]*inc
-            stats["gifs"] += (lambda x : inc if x else 0)(message["gif"])
-            # if inc == 1 and message["gif"]:
-            #     print(message["content"])
+            stats["message_count"] += inc
+            stats["image_count"] += message["num_attachments"]*inc
+
+            if message["gif"]:
+                giflink = re.match(r"https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&\/\/=]*)", message["content"])
+                if giflink is not None:
+                    stats["gif_count"] += inc
+                    giflink = giflink.group(0)
+
+                    if giflink not in stats["gifs"]:
+                        stats["gifs"][giflink] = inc
+                    else:
+                        stats["gifs"][giflink] += inc
 
             for mention in message["mentions"]:
                 if mention not in stats["mentions"]:
@@ -173,16 +197,16 @@ def generate_stats(search, messages, user):
                 stats["authors"][message["author"]] += inc
 
             if date.date() not in stats["dates"]:
-                stats["dates"][date.date()] = inc
+                stats["dates"][date.date()] = inc*wc
             else:
-                stats["dates"][date.date()] += inc
+                stats["dates"][date.date()] += inc*wc
 
-    stats["mentions"] = sorted(stats["mentions"].items(), key=lambda x:x[1], reverse=True)[0]
-    stats["authors"] = sorted(stats["authors"].items(), key=lambda x:x[1], reverse=True)[0]
-    stats["dates"] = sorted(stats["dates"].items(), key=lambda x:x[1], reverse=True)[0]
+    stats["mentions"] = sorted(stats["mentions"].items(), key=lambda x:x[1], reverse=True)
+    stats["authors"] = sorted(stats["authors"].items(), key=lambda x:x[1], reverse=True)
+    stats["dates"] = sorted(stats["dates"].items(), key=lambda x:x[1], reverse=True)
+    stats["gifs"] = sorted(stats["gifs"].items(), key=lambda x:x[1], reverse=True)
 
     return stats
-
 
 def utc_to_est(date):
     global from_zone
@@ -193,7 +217,7 @@ def utc_to_est(date):
 
 async def date_handler(interaction: discord.Interaction, date1, date2):
     if date1 is None or date2 is None:
-        await interaction.response.send_message(content="It seems you've entered the date in the wrong format. Try MM-dd-YYYY or MM/dd/YYYY", ephemeral=True)
+        await interaction.followup.send(content="It seems you've entered the date in the wrong format. Try MM-dd-YYYY or MM/dd/YYYY", ephemeral=True)
         return None
     elif date1 == "All" and date2 == "All":
         dates = [datetime.datetime(2019,11,14,0,0,0)+datetime.timedelta(days=x) for x in range((datetime.datetime.today().replace(hour=0, minute=0, second=0, microsecond=0)+datetime.timedelta(days=1)-datetime.datetime(2019,11,14,0,0,0)).days)]
@@ -204,7 +228,7 @@ async def date_handler(interaction: discord.Interaction, date1, date2):
     else:
         dates = [date1+datetime.timedelta(days=x) for x in range((date2+datetime.timedelta(days=1)-date1).days)]
     if len(dates) == 0:
-        await interaction.response.send_message(content="You flipped your start-date and end-date! Fix that, why don't you!?", ephemeral=True)
+        await interaction.followup.send(content="You flipped your start-date and end-date! Fix that, why don't you!?", ephemeral=True)
         return None
     return dates
 
