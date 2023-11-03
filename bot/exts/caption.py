369035a6d62b5e2616a6fade187ebca283aded70
Vaughn Woerpel <vaughnwoerpel@gmail.com>
2023-11-02 20:17:30 -0400

    updated a few logic items

--- bot/exts/caption.py
+++ bot/exts/caption.py
@@ -8,11 +8,13 @@
 
 # built-in
 import logging
-import re
 import math
+import re
+import traceback
 
 # external
 import discord
+from discord import app_commands
 from discord.ext import commands
 from wand.color import Color
 from wand.font import Font
@@ -24,85 +26,99 @@ from bot.utils import file_helper
 log = logging.getLogger("caption")
 
 
+class AdvancedCaption(discord.ui.Modal, title="Caption"):
+    name = discord.ui.TextInput(
+        label="Caption",
+        placeholder="Your caption here...",
+    )
+
+    async def on_submit(self, interaction: discord.Interaction):
+        await interaction.response.send_message(
+            f"Thanks for your feedback", ephemeral=True
+        )
+
+    async def on_error(
+        self, interaction: discord.Interaction, error: Exception
+    ) -> None:
+        log.error("Advanced caption modal unexpectedly errored out")
+        await interaction.response.send_message(
+            "Oops! Something went wrong.", ephemeral=True
+        )
+
+
 class Caption(commands.Cog):
     """Caption class to handle all caption requests"""
 
     def __init__(self, bot: commands.Bot) -> None:
         """Intializes the caption class"""
         self.bot = bot
+        self.advanced_caption = app_commands.ContextMenu(
+            name="Advanced Caption", callback=self.advanced_caption_menu
+        )
+        self.bot.tree.add_command(self.advanced_caption)
+
+    async def advanced_caption_menu(
+        self, interaction: discord.Interaction, message: discord.Message
+    ) -> None:
+        await interaction.response.send_modal(AdvancedCaption())
 
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message) -> None:
         """On message if someone says 'caption' it adds the caption to the image it's replying to"""
 
-        response = await caption_helper(message, self.bot.user.id)
+        if (
+            not message.content.startswith("caption")
+            or message.author.id == self.bot.user.id
+        ):
+            return
 
-        match response:
-            case "Message sent by self":
-                return
-            case "Message doesn't start with caption":
-                return
-            case "Message is not a reply":
-                return
-            case "No caption was specified":
-                await message.reply("Looks like you didn't add a caption, buddy")
-                return
-            case "Original message had no file":
-                await message.reply("Are you dumb? that's not even a file")
-                return
-            case "Invalid filetype":
-                await message.reply("That's not an image or a gif :/")
-                return
-            case "Caption failure":
-                log.error(f"Failure while trying to caption image/gif")
-                await message.reply("Caption has mysteriously failed")
-                return
+        # Get original message if it is actually a message reply
+        try:
+            original_message = await message.channel.fetch_message(
+                message.reference.message_id
+            )
+        except AttributeError:
+            return
+        fname = file_helper.grab(original_message)
+
+        # Gets caption text
+        caption_text = re.sub(r"^caption", "", message.content).strip()
+        if caption_text is None or len(caption_text) == 0:
+            await message.reply("Looks like you didn't add a caption, buddy")
+            return None
+
+        response = await caption_helper(message, fname, caption_text)
 
         log.info(f"Image was succesfully captioned: {response})")
         await message.reply(file=discord.File(response))
         file_helper.remove(response)
 
 
-async def caption_helper(message: discord.Message, bot_id: int) -> str:
+async def caption_helper(
+    message: discord.Message, fname: str, caption_text: str
+) -> str:
     """Helper method for captioning, allows for testing"""
 
-    # Checks for the author being the bot
-    if message.author.id == bot_id:
-        return "Message sent by self"
-
-    # Checks for the caption keyword
-    if not message.content.startswith("caption"):
-        return "Message doesn't start with caption"
-
-    # Get original message if it is actually a message reply
-    try:
-        original_message = await message.channel.fetch_message(
-            message.reference.message_id
-        )
-    except AttributeError:
-        return "Message is not a reply"
-
-    # Gets caption text
-    caption_text = re.sub(r"^caption", "", message.content).strip()
-    if caption_text is None or len(caption_text) == 0:
-        return "No caption was specified"
-
     # Grabs and checks file
-    fname = file_helper.grab(original_message)
+
     if fname is None:
-        return "Original message had no file"
+        await message.reply("Are you dumb? that's not even a file")
+        return None
 
     # Checks filetype
     if not fname.endswith((".png", ".jpg", ".gif", ".jpeg")):
         file_helper.remove(fname)
-        return "Invalid filetype"
+        await message.reply("That's not an image or a gif :/")
+        return None
 
     try:
         captioned = await caption(fname, caption_text)
         return captioned
     except Exception:
         file_helper.remove(fname)
-        return "Caption failure"
+        log.error(f"Failure while trying to caption image/gif")
+        await message.reply("Caption has mysteriously failed")
+        return None
 
 
 async def caption(fname: str, caption_text: str) -> str:
@@ -153,6 +169,7 @@ async def caption(fname: str, caption_text: str) -> str:
             template_image.save(filename=fname)
 
     return fname
+
 
 async def setup(bot: commands.Bot) -> None:
     """Sets up the cog"""
