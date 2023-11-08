c0fa7f0b1146023e4abc8d8f268596ecace0f25d
Vaughn Woerpel <vaughnwoerpel@gmail.com>
2023-11-08 14:03:56 -0500

    Updating the speed changer

--- bot/exts/caption.py
+++ bot/exts/caption.py
@@ -6,6 +6,7 @@
     Made with love and care by Vaughn Woerpel
 """
 
+
 import io
 
 # built-in
@@ -50,21 +51,73 @@ class SelectFont(discord.ui.Select):
 
 
 class AdvancedCaptionView(discord.ui.View):
-    def __init__(self, fname, caption_text, *, timeout=180):
+    def __init__(self, fname, *, timeout=180):
         self.fname = fname
         self.font = None
-        self.caption_text = caption_text
+        self.reversed = False
+        self.caption_text = None
+        self.playback_speed = float(1)
         super().__init__(timeout=timeout)
-        self.add_item(SelectFont())
+        self.speed_display_button.disabled = True
+        if not self.fname.endswith("gif"):
+            # self.reverse_button.disabled = True
+            self.remove_item(self.reverse_button)
 
-    @discord.ui.button(label="Preview", style=discord.ButtonStyle.blurple, row=1)
+    @discord.ui.button(label="Preview", style=discord.ButtonStyle.blurple, row=4)
     async def preview_button(
         self, interaction: discord.Interaction, button: discord.ui.Button
     ):
         await interaction.response.defer()
         await self.edit_helper(interaction)
 
-    @discord.ui.button(label="Done", style=discord.ButtonStyle.green, row=1)
+    @discord.ui.button(label="Caption", style=discord.ButtonStyle.blurple, row=1)
+    async def caption_button(
+        self, interaction: discord.Interaction, button: discord.ui.Button
+    ):
+        await interaction.response.send_modal(AdvancedCaptionModal(self))
+
+        if SelectFont not in self.children:
+            self.add_item(SelectFont())
+            await interaction.message.edit(view=self)
+
+    @discord.ui.button(label="⏪", style=discord.ButtonStyle.blurple, row=2)
+    async def slow_down_button(
+        self, interaction: discord.Interaction, button: discord.ui.Button
+    ):
+        await interaction.response.defer()
+        self.playback_speed /= 2
+        self.speed_display_button.label = f"{self.playback_speed}x"
+        await interaction.message.edit(view=self)
+
+    @discord.ui.button(label="1.0x", style=discord.ButtonStyle.gray, row=2)
+    async def speed_display_button(
+        self, interaction: discord.Interaction, button: discord.ui.Button
+    ):
+        return
+
+    @discord.ui.button(label="⏩", style=discord.ButtonStyle.blurple, row=2)
+    async def speed_up_button(
+        self, interaction: discord.Interaction, button: discord.ui.Button
+    ):
+        await interaction.response.defer()
+        self.playback_speed *= 2
+        self.speed_display_button.label = f"{self.playback_speed}x"
+        await interaction.message.edit(view=self)
+
+    @discord.ui.button(label="🔁", style=discord.ButtonStyle.gray, row=2)
+    async def reverse_button(
+        self, interaction: discord.Interaction, button: discord.ui.Button
+    ):
+        if self.reversed:
+            self.reversed = False
+            button.style = discord.ButtonStyle.gray
+            await interaction.response.edit_message(view=self)
+        else:
+            self.reversed = True
+            button.style = discord.ButtonStyle.red
+            await interaction.response.edit_message(view=self)
+
+    @discord.ui.button(label="Done", style=discord.ButtonStyle.green, row=4)
     async def go_button(
         self, interaction: discord.Interaction, button: discord.ui.Button
     ):
@@ -82,15 +135,19 @@ class AdvancedCaptionView(discord.ui.View):
         temp_filename = self.fname.split(".")[0] + "temp." + self.fname.split(".")[1]
         shutil.copy2(self.fname, temp_filename)
         captioned_file = await caption(
-            fname=temp_filename, caption_text=self.caption_text, font=self.font
+            fname=temp_filename,
+            caption_text=self.caption_text,
+            font=self.font,
+            reversed=self.reversed,
+            playback_speed=self.playback_speed,
         )
         await interaction.message.edit(attachments=[discord.File(captioned_file)])
         file_helper.remove(temp_filename)
 
 
-class AdvancedCaption(discord.ui.Modal, title="Caption"):
-    def __init__(self, fname, timeout=180):
-        self.fname = fname
+class AdvancedCaptionModal(discord.ui.Modal, title="Caption"):
+    def __init__(self, view, timeout=180):
+        self.view = view
         super().__init__(timeout=timeout)
 
     caption = discord.ui.TextInput(
@@ -100,11 +157,7 @@ class AdvancedCaption(discord.ui.Modal, title="Caption"):
 
     async def on_submit(self, interaction: discord.Interaction):
         await interaction.response.defer()
-
-        await interaction.followup.send(
-            file=discord.File(self.fname),
-            view=AdvancedCaptionView(self.fname, self.caption.value),
-        )
+        self.view.caption_text = self.caption.value
 
     async def on_error(
         self, interaction: discord.Interaction, error: Exception
@@ -121,12 +174,12 @@ class Caption(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
         """Intializes the caption class"""
         self.bot = bot
-        self.advanced_caption = app_commands.ContextMenu(
-            name="Advanced Caption", callback=self.advanced_caption_menu
+        self.advanced_edit = app_commands.ContextMenu(
+            name="Advanced Edit", callback=self.advanced_edit_menu
         )
-        self.bot.tree.add_command(self.advanced_caption)
+        self.bot.tree.add_command(self.advanced_edit)
 
-    async def advanced_caption_menu(
+    async def advanced_edit_menu(
         self, interaction: discord.Interaction, message: discord.Message
     ) -> None:
         # Grabs and checks file
@@ -145,7 +198,10 @@ class Caption(commands.Cog):
             )
             return
 
-        await interaction.response.send_modal(AdvancedCaption(fname))
+        await interaction.response.send_message(
+            file=discord.File(fname),
+            view=AdvancedCaptionView(fname),
+        )
 
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message) -> None:
@@ -213,7 +269,13 @@ async def caption_helper(message: discord.Message, caption_text: str) -> str:
         return "Caption failure"
 
 
-async def caption(fname: str, caption_text: str, font: str | None = None) -> str:
+async def caption(
+    fname: str,
+    caption_text: str,
+    font: str | None = None,
+    reversed: bool | None = False,
+    playback_speed: float | None = 1.0,
+) -> str:
     """Adds a caption to images and gifs with image_magick"""
 
     foreground = PILImage.open(fname)
@@ -252,23 +314,44 @@ async def caption(fname: str, caption_text: str, font: str | None = None) -> str
 
     if foreground.is_animated:
         frames = []
+        durations = []
         for frame in ImageSequence.Iterator(foreground):
             captioned = background.copy()
             captioned.paste(frame, (0, bar_height))
             frames.append(captioned)
+            durations.append(20)
+
+        if reversed:
+            frames.reverse()
+
+        # print(durations)
 
         frames[0].save(
             fname,
             save_all=True,
             append_images=frames,
             loop=0,
-            duration=foreground.info["duration"],
+            duration=1,
         )
     else:
         background.paste(foreground, (0, bar_height))
         background.save(fname)
 
     return fname
+
+
+def get_avg_fps(PIL_Image_object):
+    """Returns the average framerate of a PIL Image object"""
+    PIL_Image_object.seek(0)
+    frames = duration = 0
+    while True:
+        try:
+            frames += 1
+            duration += PIL_Image_object.info["duration"]
+            PIL_Image_object.seek(PIL_Image_object.tell() + 1)
+        except EOFError:
+            return frames / duration * 1000
+    return None
 
 
 async def setup(bot: commands.Bot) -> None:
