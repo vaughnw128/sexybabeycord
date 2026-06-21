910780062a3d416a01d03de8d7ed057e163aa73b
Vaughn Woerpel <vaughnwoerpel@gmail.com>
2026-06-21 12:44:34 -0400

    fix

--- src/sexybabeycord/exts/goke.py
+++ src/sexybabeycord/exts/goke.py
@@ -1,6 +1,6 @@
 # built-in
 import logging
-import re
+import random
 
 # external
 import discord
@@ -8,8 +8,18 @@ from discord.ext import commands
 
 log = logging.getLogger("gloke")
 
-gloke_regex = re.compile(r"\bgloke\b", re.IGNORECASE)
-correction_message = "I think you mean goke!"
+correction_message = "Hey there! It looks like you mentioned 'gloke' in some form. I think you meant GOKE!"
+epithets = (
+    "Goke will cleanse us all with his holy light!",
+    "Glory to goke!",
+    "Hail goke!",
+    "All heretics shall be purged in the name of goke!",
+    "Bow before goke's glory!",
+    "Goke will save us all!",
+    "Goke is the one true god!",
+    "Goke's light will guide us!",
+    "Goke will protect us from the darkness!",
+)
 
 
 class Gloke(commands.Cog):
@@ -28,10 +38,15 @@ class Gloke(commands.Cog):
         if message.author.bot:
             return
 
+        found = False
         for keyword in ("gloke", "gl0ke", "g1oke", "g10ke", "g10k3", "gl0k3", "g1ok3", "gloak", "g1oak"):
             if keyword in message.content.lower().replace(" ", ""):
                 log.debug(f"Gloke keyword '{keyword}' detected from {message.author}")
+                found = True
                 break
+
+        if not found:
+            return
 
         log.debug(f"Gloke keyword detected from {message.author}")
 
@@ -43,7 +58,7 @@ class Gloke(commands.Cog):
             return
 
         try:
-            await message.author.send(correction_message)
+            await message.author.send(correction_message + " " + epithets[random.randint(0, len(epithets) - 1)])
             log.debug(f"Sent goke correction to {message.author}")
         except discord.Forbidden:
             await message.channel.send(f"{message.author.mention} {correction_message}", delete_after=30)
